df373400610a18f5eba123dacf38ebb515fa2921
bayamasa <mapi0213mgt@gmail.com>
2021-11-17 16:58:35 +0900

    done 05-01

---
+++ module05/ex01/Form.hpp
@@ -0,0 +1,43 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+#include <string>
+#include <exception>
+
+class Form;
+#include "Bureaucrat.hpp"
+
+# define HIGHEST_GRADE 1
+# define LOWEST_GRADE 150
+
+class Form
+{
+private:
+	const std::string _name;
+	const int _gradeToSign;
+	const int _gradeToExec;
+	bool _beSigned;
+public:
+	Form();
+	~Form();
+	Form(std::string name, int gradeToSign, int gradeToExec);
+	Form(const Form &other);
+	Form &operator=(const Form &other);
+	void beSigned(Bureaucrat &bureaucrat);
+	const std::string &getName() const;
+	const int &getGradeToSign() const;
+	const int &getGradeToExec() const;
+	bool getBeSigned() const;
+	class GradeTooHighException : public std::exception {
+	public:
+		const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+	public:
+		const char *what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &os, Form const &other);
+
+#endif
