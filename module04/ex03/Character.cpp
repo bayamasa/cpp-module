7b3100ffc90b76478e86481de65af5495747eadf
bayamasa <mapi0213mgt@gmail.com>
2022-05-17 20:28:41 +0900

    double free とleakに対応

--- module04/ex03/Character.cpp
+++ module04/ex03/Character.cpp
@@ -1,18 +1,43 @@
 #include "Character.hpp"
 
+Character::Character()
+{
+	_name = "default";
+	for (int i = 0; i < INVENTORY_SIZE; i++)
+	{
+		inventory[i] = NULL;
+	}
+}
+
 Character::Character(std::string name)
 {
 	_name = name;
+	for (int i = 0; i < INVENTORY_SIZE; i++)
+	{
+		inventory[i] = NULL;
+	}
 }
 
 Character::~Character()
 {
+	for (int i = 0; i < INVENTORY_SIZE; i++)
+	{
+		if (inventory[i] != NULL)
+		{
+			delete inventory[i];
+		}
+	}
 }
 
 Character::Character(const Character &other)
 {
+	for (int i = 0; i < INVENTORY_SIZE; i++)
+	{
+		inventory[i] = NULL;
+	}
 	*this = other;
 }
+
 Character &Character::operator=(const Character &other)
 {
 	if (this != &other)
@@ -20,18 +45,25 @@ Character &Character::operator=(const Character &other)
 		_name = other._name;
 		for (int i = 0; i < INVENTORY_SIZE; i++)
 		{
+			if (inventory[i] != NULL)
+			{
+				delete inventory[i];
+				inventory[i] = NULL;
+			}
 			if (other.inventory[i] != NULL)
 			{
-				inventory[i] = other.inventory[i];
+				inventory[i] = other.inventory[i]->clone();
 			}
 		}
 	}
 	return *this;
 }
+
 std::string const&	Character::getName() const
 {
 	return _name;
 }
+
 void Character::equip(AMateria* m)
 {
 	if (m == NULL)
@@ -43,7 +75,7 @@ void Character::equip(AMateria* m)
 	{
 		if(inventory[i] == NULL)
 		{
-			inventory[i] = m;
+			inventory[i] = m->clone();
 			return;
 		}
 	}
@@ -57,6 +89,7 @@ void Character::unequip(int idx)
 	}
 	if(inventory[idx] != NULL)
 	{
+		delete inventory[idx];
 		inventory[idx] = NULL;
 	}
 }
