aefb5dc33cd789c07bf6c776aad2f087fea49ba3
bayamasa <mapi0213mgt@gmail.com>
2022-05-26 21:37:01 +0900

    done ex02

--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -3,6 +3,7 @@
 #include "C.hpp"
 #include "Base.hpp"
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 
@@ -13,12 +14,13 @@ void identify(Base& p);
 int main(void)
 {
 	identify(generate());
+	identify(*generate());
 }
 
 Base * generate(void)
 {
-	int seed = rand(time(NULL)) % 3;
-	std::cout << "seed: " << seed << std::endl;
+	srand(time(NULL));
+	int seed = rand() % 3;
 	if (seed == 0)
 		return new A();
 	else if (seed == 1)
@@ -40,7 +42,24 @@ void identify(Base* p)
 		std::cout << "This is C" << std::endl;
 }
 
-// void identify(Base& p)
-// {
-	
-// }
+void identify(Base& p)
+{
+	try
+	{
+		A a = dynamic_cast<A&>(p);
+		std::cout << "This is A" << std::endl;
+	}
+	catch(const std::exception& e){}
+	try
+	{
+		B b = dynamic_cast<B&>(p);
+		std::cout << "This is B" << std::endl;
+	}
+	catch(const std::exception& e){}
+	try
+	{
+		C c = dynamic_cast<C&>(p);
+		std::cout << "This is C" << std::endl;
+	}
+	catch(const std::exception& e){}
+}
