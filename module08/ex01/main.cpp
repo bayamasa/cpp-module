1a9f2b591c9b6d5a98aa987698ad9cdaa8007f08
bayamasa <mapi0213mgt@gmail.com>
2022-05-30 12:57:06 +0900

    test追加

--- module08/ex01/main.cpp
+++ module08/ex01/main.cpp
@@ -13,5 +13,15 @@ int main(void)
 	sp.addNumber(11);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
-	return 0;
+	
+	Span sp_2 = Span(2);
+	int array[] = {2, 3};
+	std::vector<int> v(array, array + 2);
+	std::cout << "v.size(): " << v.size() << std::endl;
+	std::cout << "v.at(0): " << v.at(0) << std::endl;
+	std::cout << "v.at(1): " << v.at(1) << std::endl;
+	sp_2.addNumbers(v);
+	std::cout << sp_2.shortestSpan() << std::endl;
+	std::cout << sp_2.longestSpan() << std::endl;
+	system("leaks a.out");
 }
