8edd7e8ac60a52e8131e1e038a3ba55f4d0b2077
bayamasa <mapi0213mgt@gmail.com>
2022-05-30 12:33:34 +0900

    最大値、最小値test

--- module08/ex01/main.cpp
+++ module08/ex01/main.cpp
@@ -8,8 +8,8 @@ int main(void)
 	Span sp = Span(5);
 	sp.addNumber(6);
 	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
+	sp.addNumber(INT32_MAX);
+	sp.addNumber(INT32_MIN);
 	sp.addNumber(11);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
